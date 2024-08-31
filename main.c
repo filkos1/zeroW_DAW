cea7454b89a2c85dbfb84418882d6f5df45e3d49
filkos1 <ziufilip@gmail.com>
2024-08-31 22:32:46 +0200

    AutomatedCommit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>  // Include the standard input/output header
+
+// The main function - entry point of the program
+int main(int argc, char *argv[]) {
+    // Print a message to the console
+    printf("Hello, World!\n");
+
+    // Your code here
+
+    // Return 0 to indicate successful completion
+    return 0;
+}
