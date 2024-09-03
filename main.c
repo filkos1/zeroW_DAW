e17ec7049c9b2da4a1807b643070e4328edf3abe
filkos1 <ziufilip@gmail.com>
2024-09-03 14:51:29 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,9 @@
-#include <stdio.h>  // Include the standard input/output header
+#include <stdio.h>  // Include the standard input/output
 
-// The main function - entry point of the program
+// The main function
 int main(int argc, char *argv[]) {
-    // Print a message to the console
+    
     printf("Hello, World!\n");
-
-    // Your code here
 
     // Return 0 to indicate successful completion
     return 0;
