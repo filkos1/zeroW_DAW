1b67c4b347e987fc87b6db735861b5a8a9a38eb6
filkos1 <ziufilip@gmail.com>
2024-09-08 14:01:47 +0200

    AutomatedCommit

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>  // Include the standard input/output
+#include <bcm2835.h>
+
 
 // The main function
 int main(int argc, char *argv[]) {
     
+    if (!bcm2835_init()) {
+        printf("bcm2835_init failed. Are you running as root?\n");
+        return 1;
+    }
+
     printf("Hello, World!\n");
 
     // Return 0 to indicate successful completion
