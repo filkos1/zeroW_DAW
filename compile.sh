9c4a32c4413ec1e7e1f0315db4fb28b4cc6e851c
filkos1 <ziufilip@gmail.com>
2024-09-14 14:01:29 +0200

    AutomatedCommit

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #in this hell script there will be all shell C compiler commands and maybe some misc stuff
 
 echo "Compiling with an output in $1"
-gcc -o "$1" ./main.c -lbcm2835
+gcc -o "$1" ./main.c -lbcm2835 -lwiringPiDev
--- main.c
+++ main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>  // Include the standard input/output
 #include <bcm2835.h>
-
+#include <wiringPi.h>
 
 // The main function
 int main(int argc, char *argv[]) {
-    
+
+    if (wiringPiSetup() == -1) {
+        printf("WiringPi init failed\n")
+        return 2; // Initialization failed
+    }
+
     if (!bcm2835_init()) {
         printf("bcm2835_init failed. Are you running as root?\n");
         return 1;
     }
+
+
 
     printf("Hello, World!\n");
 
