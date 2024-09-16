3422b01960ac12378a66c885bfe8c96f032f1233
filkos1 <ziufilip@gmail.com>
2024-09-16 13:07:43 +0200

    AutomatedCommit

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 //libraries
 #include <stdio.h>  
 #include <wiringPi.h>
@@ -16,7 +15,7 @@
 #define MAX_PRESSED 3  // Maximum number of presses we track at one time
 
 //values
-unsigned char pressed[MAX_PRESSED] = {0};
+unsigned char pressed[MAX_PRESSED] = {0};  // Array to store pressed buttons
 unsigned char nextArr = 0;
 
 
@@ -27,9 +26,9 @@ int main(int argc, char *argv[]) {
         printf("WiringPi init failed\n");
         return 2; // Initialization failed
     }
-    printf("\033[32mInit succeded\033[37m\n");
+    printf("\033[32mInit succeeded\033[37m\n");
     
-    //pin modes
+    // Pin modes
     pinMode(O1, OUTPUT);
     pinMode(O2, OUTPUT);
     pinMode(O3, OUTPUT);
@@ -49,74 +48,62 @@ int main(int argc, char *argv[]) {
         }
         nextArr = 0;  // Reset the index for the current scan
 
-        //row 1
+        // Check Row 1
         digitalWrite(O1, HIGH);
-        delay(10);
-        if (digitalRead(I1) == HIGH) {
-            pressed[nextArr] = 11;
-            nextArr ++;
+        delay(10);  // Slight delay for stable read
+        if (digitalRead(I1) == HIGH && nextArr < MAX_PRESSED) {
+            pressed[nextArr++] = 11;  // Row 1, Column 1
         }
-        if (digitalRead(I2) == HIGH) {
-            pressed[nextArr] = 12;
-            nextArr ++;
+        if (digitalRead(I2) == HIGH && nextArr < MAX_PRESSED) {
+            pressed[nextArr++] = 12;  // Row 1, Column 2
         }
-        if (digitalRead(I3) == HIGH) {
-            pressed[nextArr] = 13;
-            nextArr ++;
+        if (digitalRead(I3) == HIGH && nextArr < MAX_PRESSED) {
+            pressed[nextArr++] = 13;  // Row 1, Column 3
         }
-        digitalWrite(O1, LOW);
+        digitalWrite(O1, LOW);  // Turn off Row 1
 
-        //row 2
+        // Check Row 2
         digitalWrite(O2, HIGH);
-        delay(10);
-        if (digitalRead(I1) == HIGH) {
-            pressed[nextArr] = 21;
-            nextArr ++;
+        delay(10);  // Slight delay for stable read
+        if (digitalRead(I1) == HIGH && nextArr < MAX_PRESSED) {
+            pressed[nextArr++] = 21;  // Row 2, Column 1
         }
-        if (digitalRead(I2) == HIGH) {
-            pressed[nextArr] = 22;
-            nextArr ++;
+        if (digitalRead(I2) == HIGH && nextArr < MAX_PRESSED) {
+            pressed[nextArr++] = 22;  // Row 2, Column 2
         }
-        if (digitalRead(I3) == HIGH) {
-            pressed[nextArr] = 23;
-            nextArr ++;
+        if (digitalRead(I3) == HIGH && nextArr < MAX_PRESSED) {
+            pressed[nextArr++] = 23;  // Row 2, Column 3
         }
-        digitalWrite(O2, LOW);
+        digitalWrite(O2, LOW);  // Turn off Row 2
 
-
-
+        // Check Row 3
         digitalWrite(O3, HIGH);
-        delay(10);
-        if (digitalRead(I1) == HIGH) {
-            pressed[nextArr] = 31;
-            nextArr ++;
+        delay(10);  // Slight delay for stable read
+        if (digitalRead(I1) == HIGH && nextArr < MAX_PRESSED) {
+            pressed[nextArr++] = 31;  // Row 3, Column 1
         }
-        if (digitalRead(I2) == HIGH) {
-            pressed[nextArr] = 32;
-            nextArr ++;
+        if (digitalRead(I2) == HIGH && nextArr < MAX_PRESSED) {
+            pressed[nextArr++] = 32;  // Row 3, Column 2
         }
-        if (digitalRead(I3) == HIGH) {
-            pressed[nextArr] = 33;
-            nextArr ++;
+        if (digitalRead(I3) == HIGH && nextArr < MAX_PRESSED) {
+            pressed[nextArr++] = 33;  // Row 3, Column 3
         }
-        digitalWrite(O3, LOW);
-         
-        printf("\r\033[KPressed buttons are ");
+        digitalWrite(O3, LOW);  // Turn off Row 3
+
+        // Clear the line and print results
+        printf("\r\033[KPressed buttons are: ");
         for (int i = 0; i < nextArr; i++) {
-            printf("%d ", pressed[i]);
+            printf("%d ", pressed[i]);  // Print valid button presses
         }
+
+        // Ensure clearing of previous longer output
         printf("                     ");
         
-        fflush(stdout); 
+        fflush(stdout);  // Ensure immediate display
 
-        delay(100);
-
+        delay(100);  // Slight delay between iterations
     }
-    
 
-   
-    
-    
     // Return 0 to indicate successful completion
     return 0;
 }
